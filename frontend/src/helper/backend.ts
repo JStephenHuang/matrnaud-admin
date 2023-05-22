fd88a8db61c53ec3e4e83c05686a931ba4ef5960
JStephenHuang <jstephhuang@gmail.com>
2023-05-22 16:30:30 -0400

    Expanded Frontend

--- frontend/src/helper/backend.ts
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
-
-export const backend = axios.create({
-  baseURL: backendUrl,
-  headers: { "Access-Control-Allow-Origin": backendUrl },
-});

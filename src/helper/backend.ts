997a6dfb54459c2174713d3a2a210d18de8c52b7
JStephenHuang <jstephhuang@gmail.com>
2023-05-22 22:28:46 -0400

    Vercel config

--- src/helper/backend.ts
+++ src/helper/backend.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const backendUrl = import.meta.env.VITE_BACKEND_URL;
+const backendUrl = import.meta.env.VERCEL_URL;
 
 export const backend = axios.create({
   baseURL: backendUrl,
